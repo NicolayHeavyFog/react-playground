951826efbb6d53d23cd01a62be530a9651eb31ac
NicolayHeavyFog <nikolay_on_parseltongue@mail.ru>
2022-09-15 21:11:43 +0700

    ssr

---
+++ src/server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import ReactDOM from "react-dom/server";
+import { Header } from "../shared/Header.jsx";
+import { indexTemplate } from "./indexTemplate.js";
+
+const app = express();
+
+app.use("/static", express.static("./dist/client"));
+
+app.get("/", (req, res) => {
+  res.send(indexTemplate(ReactDOM.renderToString(Header())));
+});
+
+app.listen(3000, () => {
+  console.log("server statred on http://localhost:3000");
+});
---
+++ src/shared/Header.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+// class Header extends React.Component {
+//   render() {
+//     <header>
+//       <h1>Reddit on my own</h1>
+//     </header>;
+//   }
+// }
+
+// export default Header;
+
+export function Header() {
+  return (
+    <header>
+      <h1>Reddit on my own 123</h1>
+    </header>
+  );
+}
